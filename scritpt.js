170a4ab9b9f090c8e7f578174a8dc75303603dc1
Josué Klaysler <josueklayslerjk@gmail.com>
2023-12-29 17:27:17 -0200

    alt JS

--- scritpt.js
+++ scritpt.js
@@ -1,35 +1,19 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light") /*essa função faz todo o trabalho que se fez com as conicionais,ou seja, essa função faz tudo no automático*/ 
+  html.classList.toggle(
+    "light"
+  ) /*essa função faz todo o trabalho que se fez com as conicionais,ou seja, essa função faz tudo no automático*/
 
   // PEGA A TAG IMG
   const img = document.querySelector("#profile img ")
-  
-  if(html.classList.contains("light")) {
-  //SUBSTITUIR A IMAGEM
-  img.setAttribute("src","./assets/avatar-light.png")
 
-
-  } 
-  else{
+  if (html.classList.contains("light")) {
+    //SUBSTITUIR A IMAGEM
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
     img.setAttribute("src", "./assets/avatar.png")
-  img.setAttribute(
-    "alt",
-    "foto de Mark Brito sorrindo, usando óculos com barba, camisa preta e fundo amarelo"
-  )
-
   }
 
-
- // SE TIVER LIGHT MODE, ADICIONA A IMAGEM LIGHT
- // SE TIVER SEM LIGHT LIGHT MODE, MANTER A IMAGEM NORMAL
-
-
-
+  // SE TIVER LIGHT MODE, ADICIONA A IMAGEM LIGHT
+  // SE TIVER SEM LIGHT LIGHT MODE, MANTER A IMAGEM NORMAL
 }
-
-
-
-
-
-
